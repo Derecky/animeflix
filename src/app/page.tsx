8268bb56649f8c89edf23e71499801bfca1893d7
Benicio <beniciojr18@gmail.com>
2026-02-24 23:52:31 -0300

    refactor(app): organize component imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Header } from "./components/Header/Header";
-import { Content } from "./components/Content/Content";
-import { Footer } from "./components/Footer/Footer";
+import { Header } from "../components/Header/Header";
+import { Content } from "../components/Content/Content";
+import { Footer } from "../components/Footer/Footer";
 
 import "./globals.css";
 
