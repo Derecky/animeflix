57c4b2dcc807340c67e86769e1674b797100fe48
Benicio <beniciojr18@gmail.com>
2026-03-05 21:16:40 -0300

    feat(filter): implement category navigation with horizontal scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../component/Header/Header";
+import { Filter } from "../component/Filter/Filter";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Filter />
     </>
   );
 }
