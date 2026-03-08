9dd325eae03d921d6bdaa02fc228473268e1bd7e
Benicio <beniciojr18@gmail.com>
2026-03-08 14:58:43 -0300

    feat(home): render content component on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../component/Header/Header";
 import { Filter } from "../component/Filter/Filter";
+import { Content } from "../component/Content/Content";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Filter />
+      <Content />
     </>
   );
 }
