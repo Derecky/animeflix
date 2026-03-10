b2476d87fc80f4dc3709d49a8435992cb4dde28a
Benicio <beniciojr18@gmail.com>
2026-03-09 21:51:06 -0300

    refactor(page): update content component import to default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../component/Header/Header";
 import { Filter } from "../component/Filter/Filter";
-import { Content } from "../component/Content/Content";
+import Content from "../component/Content/Content";
 
 export default function Home() {
   return (
