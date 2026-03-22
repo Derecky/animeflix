2ca41ecf8210c5aee1b7ace40179302886caa799
Benicio <beniciojr18@gmail.com>
2026-03-22 19:40:26 -0300

    refactor(filter): render links from mapped array

--- src/component/Filter/Filter.tsx
+++ src/component/Filter/Filter.tsx
@@ -1,91 +1,21 @@
 import styles from "./Filter.module.css";
 
+const filters = ["Todos", ..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
 export function Filter() {
   return (
     <header className={styles.FilterContainer}>
       <nav className={styles.navbar}>
-        <a href="#" className={styles.navlink + " " + styles.todos}>
-          Todos
-        </a>
-        <a href="#" className={styles.navlink}>
-          A
-        </a>
-        <a href="#" className={styles.navlink}>
-          B
-        </a>
-        <a href="#" className={styles.navlink}>
-          C
-        </a>
-        <a href="#" className={styles.navlink}>
-          D
-        </a>
-        <a href="#" className={styles.navlink}>
-          E
-        </a>
-        <a href="#" className={styles.navlink}>
-          F
-        </a>
-        <a href="#" className={styles.navlink}>
-          G
-        </a>
-        <a href="#" className={styles.navlink}>
-          H
-        </a>
-        <a href="#" className={styles.navlink}>
-          I
-        </a>
-        <a href="#" className={styles.navlink}>
-          J
-        </a>
-        <a href="#" className={styles.navlink}>
-          K
-        </a>
-        <a href="#" className={styles.navlink}>
-          L
-        </a>
-        <a href="#" className={styles.navlink}>
-          M
-        </a>
-        <a href="#" className={styles.navlink}>
-          N
-        </a>
-        <a href="#" className={styles.navlink}>
-          O
-        </a>
-        <a href="#" className={styles.navlink}>
-          P
-        </a>
-        <a href="#" className={styles.navlink}>
-          Q
-        </a>
-        <a href="#" className={styles.navlink}>
-          R
-        </a>
-        <a href="#" className={styles.navlink}>
-          S
-        </a>
-        <a href="#" className={styles.navlink}>
-          T
-        </a>
-        <a href="#" className={styles.navlink}>
-          U
-        </a>
-        <a href="#" className={styles.navlink}>
-          V
-        </a>
-        <a href="#" className={styles.navlink}>
-          W
-        </a>
-        <a href="#" className={styles.navlink}>
-          X
-        </a>
-        <a href="#" className={styles.navlink}>
-          Y
-        </a>
-        <a href="#" className={styles.navlink}>
-          Z
-        </a>
+        {filters.map((filter) => (
+          <a
+            key={filter}
+            href="#"
+            className={`${styles.navlink} ${filter === "Todos" ? styles.todos : ""}`}
+          >
+            {filter}
+          </a>
+        ))}
       </nav>
     </header>
-  )
+  );
 }
