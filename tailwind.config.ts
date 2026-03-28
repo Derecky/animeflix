5d62802488c5a138ce444bb2266bff3b9e4c164f
Benicio <beniciojr18@gmail.com>
2026-03-28 18:04:05 -0300

    feat(tailwind): add explicit tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const config: Config = {
+  darkMode: ["class", "class"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        rubik: ["var(--font-rubik)", "sans-serif"],
+        inter: ["var(--font-inter)", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+};
+
+export default config;
