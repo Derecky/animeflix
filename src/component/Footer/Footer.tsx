9770ce75bcd4e47c57f4f2eca6b00fa3332e7bfc
Benicio <beniciojr18@gmail.com>
2026-05-05 22:10:30 -0300

    refactor: migrate footer.tsx from css modules to pure tailwind css

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,9 +1,7 @@
-import styles from "./Footer.module.css";
-
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <p className={styles.footerContent}>
+    <footer className="flex items-center justify-center">
+      <p className="font-inter mt-16 py-6 text-sm font-normal text-gray-500">
         Todos os direitos reservados aos criadores e produtores dos animes
       </p>
     </footer>
